90c1a547be47b4a177e7d3e8bc437c81d23a9def
Idan magled <idanmagled@gmail.com>
2021-11-15 18:15:49 +0200

    - Solved HW

--- 08.11-jquery/script.js
+++ 08.11-jquery/script.js
@@ -1,27 +1,50 @@
 let counter = 0;
 let contacts = [];
 function addContact(name, phone) {
+    let id = ++counter
     contacts.push({
         name,
         phone,
-        id: ++counter
+        id
     });
+    return id;
 }
 
 function createHtmlElement(name, phone) {
-    addContact(name, phone);
-    let pName = $(`<p data-role="contact-name">${name}<p>`);
-    let pPhone = $(`<p data-role="contact-phone">${phone}<p>`);
+    let contactId = addContact(name, phone);
+    let pName = $(`<p data-contact-id=${contactId} data-role="contact-name">${name}<p>`);
+    let pPhone = $(`<p data-contact-id=${contactId} data-role="contact-phone">${phone}<p>`);
     let removeButton = $('<button>Remove</button>').click(function () {
         $(this).parent().remove();
     });
     let editButton = $('<button>Edit</button>').click(function () {
         let editName = $(`<input type="text" value=${$(this).siblings("p[data-role=contact-name]").text()}>`)
-        let editPhone = $(`<input type="tel"> value=${$(this).siblings("p[data-role=contact-phone]").text()}`);
-        $(this).parent().append([editName, editPhone])
+        let editPhone = $(`<input type="tel"  value=${$(this).siblings("p[data-role=contact-phone]").text()}>`);
+        let updateButton = $(`<button >Update</button>`)
+        updateButton.click(function () {
+            updateContact(contactId, editName.val(), editPhone.val(), updateButton);
+            editName.remove()
+            editPhone.remove();
+            updateButton.remove()
+        });
+        $(this).parent().append([editName, editPhone, updateButton])
     });
     $("[data-role=contacts-container]").append($('<div>').append([pName, pPhone, removeButton, editButton]))
     $('input').val("")
+}
+function updateContact(id, name, phone, updateButton) {
+    $.each(contacts, function (i, contact) {
+        if (contact.id === id) {
+            updateUIContact(contact);
+        }
+    })
+    function updateUIContact(contact) {
+        console.log(updateButton);
+        contact.name = name;
+        contact.phone = phone;
+        $(`[data-contact-id=${id}][data-role="contact-name"]`).text(name);
+        $(`[data-contact-id=${id}][data-role="contact-phone"]`).text(phone);
+    }
 }
 $("[data-role=add-contact-btn]").click(function () {
     const name = $("[data-role=userName]").val(), phone = $("[data-role=phone]").val();
