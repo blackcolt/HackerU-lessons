86eb4e4559e6e77d25cbc981a4f771af039a6363
idan.magled <idan.magled@naturalint.com>
2022-01-13 11:36:18 +0200

    - Some routes

--- 21.13.01 - express/express-server/app.js
+++ 21.13.01 - express/express-server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const JSONStream = require('JSONStream')
+var mongojs = require('mongojs');
+var db = mongojs('0.0.0.0/node-db');
+
 const app = express();
 app.use(express.json());
 app.use(express.static('public'))
+
 app.listen(3000, () =>{
     console.info("App listning in port 3000")
 })
@@ -11,6 +16,12 @@ app.get('/home', (req, res, next)=>{
    res.json({"name": process.count++ })
 })
 
-app.post('/users', (req, res, next)=>{
-    res.send("1111");
+app.get('/reminders', (req, res, next)=>{
+    db.collection('reminders').find(function (err, docs) {
+        res.json(docs);
+    })
+})
+
+app.post('/reminders', (req, res)=>{
+    res.json(db.collection('reminders').insert(req.body));
 })
