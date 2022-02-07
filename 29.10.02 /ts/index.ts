9613e0da1722192783f4f2678299f67fb5b9bf1b
idan.magled <idan.magled@naturalint.com>
2022-02-07 22:45:29 +0200

    - Added HE

--- 29.10.02 /ts/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { Schema, model, connect } from 'mongoose';
-const app = express();
-const port = 3000;
-
-app.listen(port, () => console.log(`Timezones by location application is running on port ${port}.`));
-
