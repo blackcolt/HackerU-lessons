ca06aa6cf6cf86af16f5eb5f86d56df4d81f1cd9
idan.magled <idan.magled@naturalint.com>
2022-02-14 23:44:53 +0200

    - Changed loading logic

--- 29.10.02 - register user/register/index.js
+++ 29.10.02 - register user/register/index.js
@@ -6,7 +6,25 @@ const express = require("express"),
   port = 3000;
 
 app.use(express.static("public"));
+
 app.get("/", (req, res) => res.json({ hello: "world" }));
+
+app.get("/users", (req, res) => {
+  User.find({ age: 18 })
+    .then((users) => {
+      for (let user of users) {
+        // delete user._doc.password;
+      }
+      res.json(users);
+    })
+    .catch((err) => res.json(err));
+});
+
+app.get("/usersCanDrink", (req, res) => {
+  User.find({ age: 16 })
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
 
 app.post("/registerUser", (req, res) => {
   //   const user = new User({ name: "idan", password: "magled" });
