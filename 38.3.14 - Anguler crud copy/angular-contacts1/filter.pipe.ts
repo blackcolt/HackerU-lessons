dc8d8d8c6c823e8a3895146278df565a4d508c4a
idan.magled <idan.magled@naturalint.com>
2022-03-12 13:55:30 +0200

    - Added lesson

---
+++ 38.3.14 - Anguler crud copy/angular-contacts1/filter.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'appFilter' })
+export class FilterPipe implements PipeTransform {
+  /**
+   * Pipe filters the list of elements based on the search text provided
+   *
+   * @param items list of elements to search in
+   * @param searchText search string
+   * @returns list of elements filtered by search text or []
+   */
+  transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(it => {
+      return it.toLocaleLowerCase().includes(searchText);
+    });
+  }
+}
