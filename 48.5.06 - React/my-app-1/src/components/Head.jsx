1fe42b200c5489b0e4a3fafb5fba04d88ec53d33
idan.magled <idanmagled@gmail.com>
2022-05-11 15:14:25 +0300

    - Added lesson

---
+++ 48.5.06 - React/my-app-1/src/components/Head.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Head extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <h1>111</h1>
+      </div>
+    );
+  }
+}
